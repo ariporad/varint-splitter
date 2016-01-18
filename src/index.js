cbbd1631ca8050ff5ebed22f06a03c17f3cf3517
Ari Porad <ari@ariporad.com>
2016-01-17 16:42:05 -0800

    feat(Splitter): impliment it
    (Just copied from another project)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 /* (c) 2016 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
+import { Transform } from 'stream';
+import varint from 'varint';
+
+export default class Splitter extends Transform {
+  chunk = new Buffer(0);
+  packetLen = null;
+  state = 0;
+
+  _transform(chunk, encoding, callback) {
+    /* istanbul ignore next: pretty sure that this is just a internal (non-testable) stream thing */
+    if (encoding !== 'buffer') chunk = new Buffer(chunk, encoding); // No clue if this is right
+
+    // If we already have a partial chunk
+    if (this.chunk.length) chunk = Buffer.concat([this.chunk, chunk]);
+
+    // noinspection FallThroughInSwitchStatementJS
+    switch (this.state) {
+      case 0: // We need to figure out the length of the packet.
+        const packetLen = varint.decode(chunk);
+        if (packetLen === undefined) break; // Partial Varints
+        this.packetLen = packetLen;
+        chunk = chunk.slice(varint.decode.bytes);
+        this.state = 1;
+      // Intentional Fallthrough
+      case 1: // We have some number of chunks, do we have a full packet?
+        if (chunk.length < this.packetLen) break;
+        const packet = chunk.slice(0, this.packetLen);
+        this.push(packet); // Pass the packet through
+        // Go back to start
+        chunk = chunk.slice(this.packetLen); // The remaining chunk.
+        this.packetLen = null;
+        this.state = 0;
+        break;
+      /* istanbul ignore next: This doesn't make sense to test, nor is it easy to test */
+      default:
+        throw new Error('Unknown State: ' + this.state + '!');
+    }
+    this.chunk = chunk; // We've already checked for an existing chunk.
+
+    callback();
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,77 @@
 /* (c) 2016 Ari Porad (@ariporad) <http://ariporad.com>. License: ariporad.mit-license.org */
 import test from 'ava';
-import doSomething from '../';
+import varint from 'varint';
+import { Readable } from 'stream';
+import Splitter from '..';
 
-test(t => {
-  t.ok(true);
-  doSomething();
+const REALLY_LONG = Array(300).join('!');
+
+function makePacket(msg = 'foo bar baz') {
+  const payload = new Buffer(msg);
+  const packet = Buffer.concat([new Buffer(varint.encode(payload.length)), payload]);
+  return { payload, packet };
+}
+
+test.beforeEach('streams', t => {
+  const x = t.context;
+  // Fake Readable
+  const input = x.input = new Readable();
+  input._read = () => {};
+  x.write = ::input.push;
+
+  // The Splitter
+  const output = x.output = new Splitter();
+  input.pipe(output);
+});
+
+test.beforeEach('custom asserts', t => {
+  const x = t.context;
+
+  // Buffer Comparison
+  x.bufEqu = (actual, expected, msg) => {
+    return t.is(actual.toString('hex'), expected.toString('hex'), msg);
+  };
+  x.bufNotEqu = (actual, expected, msg) => {
+    return t.isNot(actual.toString('hex'), expected.toString('hex'), msg);
+  };
+});
+
+test.beforeEach('dealing with chunks', t => {
+  const x = t.context;
+
+  // Returns a promise which resolves to the next chunk emitted from `t.context.output`
+  x.getChunk = () => new Promise(good => x.output.once('data', good));
+
+  // Basically getChunk().then(buf(Not)Equ);
+  x.chunkEqu = (expected, msg) => x.getChunk().then(chunk => x.bufEqu(chunk, expected, msg));
+  x.chunkNotEqu = (expected, msg) => x.getChunk().then(chunk => x.bufNotEqu(chunk, expected, msg));
+});
+
+test('basics', async ({ context: { write, chunkEqu } /*, ...t */ }) => {
+  const { payload, packet } = makePacket();
+
+  write(packet.slice(0, 5));
+  write(packet.slice(5));
+
+  chunkEqu(payload, 'Packet is emitted as a single event, without the length header');
+});
+
+test('length in separate net packets', async ({ context: { write, chunkEqu }, ...t }) => {
+  // I know that I should escape the '\'s, but it doesn't actually matter.
+  const { payload, packet } = makePacket(REALLY_LONG);
+
+  t.ok(payload.length > 256, 'Payload is >256 characters'); // Sanity Check
+  write(packet.slice(0, 1));
+  write(packet.slice(1));
+  chunkEqu(payload, 'Packet is emitted as a single chunk');
+});
+
+
+test('payload in >1 packet', async ({ context: { write, chunkEqu } /*, ...t */ }) => {
+  // I know that I should escape the '\'s, but it doesn't actually matter.
+  const { payload, packet } = makePacket(REALLY_LONG);
+
+  for (let i = 0; i < packet.length; ++i) write(packet.slice(i, i + 1));
+
+  chunkEqu(payload, 'Packet is emitted as a single chunk');
 });
